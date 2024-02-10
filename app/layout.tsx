0e89dd8fc9769187d841689f8ac877c68ae325d1
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-02-10 11:43:58 -0300

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Beautiful Figma Design to Web Page',
     description: 'I just turned a beautiful Figma Design in a real web page using by Front-End skills using Next.js 14, TypeScript and Tailwind.css',
-    url: 'https://josiasbudaydeveloper-figma-web-design-to-real-web-page.vercel.app/',
-    images: [
-      {url: 'https://josiasbudaydeveloper-beautiful-figma-design-to-real-web-page.vercel.app/'}
-    ]
+    url: 'https://josiasbudaydeveloper-figma-web-design-to-real-web-page.vercel.app/'
   }
 }
 
