666e554de7989c5ac095fffe5f472913a7afb00a
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-02-10 00:02:04 -0300

    Improving OpenGraph information

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -2,6 +2,12 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: 'Properties',
+  description: "I just turned a beautiful Figma Design in a real web page using by Front-End skills using Next.js 14, TypeScript and Tailwind.css",
+  openGraph: {
+    title: 'Beautiful Figma Design to Web Page',
+    description: 'I just turned a beautiful Figma Design in a real web page using by Front-End skills using Next.js 14, TypeScript and Tailwind.css',
+    url: 'https://josiasbudaydeveloper-figma-web-design-to-real-web-page.vercel.app/'
+  }
 };
 
 export default function Properties() {
