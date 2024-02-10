5017b6846e3e06e0ca26a5f4bd49b12acf08a1de
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-02-10 11:30:28 -0300

    Another try of implementing og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Beautiful Figma Design to Web Page',
     description: 'I just turned a beautiful Figma Design in a real web page using by Front-End skills using Next.js 14, TypeScript and Tailwind.css',
-    url: 'https://josiasbudaydeveloper-figma-web-design-to-real-web-page.vercel.app/'
+    url: 'https://josiasbudaydeveloper-figma-web-design-to-real-web-page.vercel.app/',
+    images: [
+      {url: '/opengraph-image.png'}
+    ]
   }
 }
 
